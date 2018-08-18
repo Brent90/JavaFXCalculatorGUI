55526e4f4f9fa666598097496bf3f5252aa06d80
Brent90 <jbaumgart2357@gmail.com>
2018-08-18 19:13:09 -0400

    fisrt commit

---
+++ src/javafxcalculator/FXMLController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxcalculator;
+import javafx.event.ActionEvent;
+import javafx.scene.text.Text;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author slinger
+ */
+public class FXMLController {
+    
+}
